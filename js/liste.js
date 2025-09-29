e09f8b50f1221320f4755cbaa53fad5d35674a57
Anthon <anan0013@stud.kea.dk>
2025-09-29 14:00:08 +0200

    Forside gjort dynamisk

--- js/liste.js
+++ js/liste.js
@@ -1,14 +1,17 @@
- const listContainer = document.querySelector("#ProductListContainer") 
- 
- fetch(`https://kea-alt-del.dk/t7/api/products?limit=40`)
-    .then ((response) => response.json())
-    .then(showProducts);
+const params = new URLSearchParams(window.location.search);
+const category = params.get("category");
+
+const listContainer = document.querySelector("#ProductListContainer");
+const categoryHeader = document.querySelector("#CategoryHeader");
+
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=40&category=${category}`)
+  .then((response) => response.json())
+  .then(showProducts);
 
 function showProducts(data) {
- let markup = "";
- data.forEach(product =>{
-    markup +=
- `<div class="ProductCard ${product.discount && "Nedsat"} ${product.soldout && "SoldOut"}">
+  let markup = "";
+  data.forEach((product) => {
+    markup += `<div class="ProductCard ${product.discount && "Nedsat"} ${product.soldout && "SoldOut"}">
           <div>
             <div class="ImgContainer">
               <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="Et produkt" />
@@ -22,12 +25,13 @@ function showProducts(data) {
             <p class="Categories">${product.articletype} | ${product.brandname}</p>
             <p class="PrevPrice">Prev. DKK ${product.price},-</p>
             <div class="PriceDisc">
-              <p class="Price"><span> Now&nbsp;</span>DKK ${Math.round(product.price - product.price * product.discount / 100)},-</p>
+              <p class="Price"><span> Now&nbsp;</span>DKK ${Math.round(product.price - (product.price * product.discount) / 100)},-</p>
               <p class="Discount">-${product.discount}%</p>
             </div>
             <a class="SeMere" href="produkt.html?id=${product.id}">Read More</a>
           </div>
-        </div>`
-    });
-    listContainer.innerHTML += markup;
+        </div>`;
+  });
+  listContainer.innerHTML += markup;
+  categoryHeader.innerHTML = `${category}`;
 }
